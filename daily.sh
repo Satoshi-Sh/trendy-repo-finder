ffe43e3fe7d376cf86b9b567a6edf145060735a5
satoshi-sh <satoss1108@gmail.com>
2024-11-05 20:13:04 -0600

    Add datasets update for Kaggle

--- daily.sh
+++ daily.sh
@@ -2,3 +2,7 @@
 cd ~/projects/trendy-repo-finder
 source venv/bin/activate
 python scraper/scrape.py
+
+current_datetime=$(date '+%Y-%m-%d')
+#upload dataset to the kaggle
+kaggle datasets version --dir-mode zip -p data -m "Dataset updated on ${current_datetime}"
