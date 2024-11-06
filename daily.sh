b1bebbb95794a221f75858d7236a19f62e38e4f1
satoshi-sh <satoss1108@gmail.com>
2024-11-06 13:57:45 -0600

    Add loggin on bash script

--- daily.sh
+++ daily.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "Starting script at $(date)" >> /var/log/trendy.log
 cd ~/projects/trendy-repo-finder
 source venv/bin/activate
 python scraper/scrape.py
 
 current_datetime=$(date '+%Y-%m-%d')
-#upload dataset to the kaggle
+echo "Dataset update time: $current_datetime" >> /var/log/trendy.log
 kaggle datasets version --dir-mode zip -p data -m "Dataset updated on ${current_datetime}"
+echo "Script finished at $(date)" >> /var/log/trendy.log
