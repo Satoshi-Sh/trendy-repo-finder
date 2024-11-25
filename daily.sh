65239a5d98d023fdf61d47f5b809aec099e0dc90
satoshi-sh <satoss1108@gmail.com>
2024-11-25 17:42:35 -0600

    Update daily.sh and .gitignore

--- daily.sh
+++ daily.sh
@@ -6,5 +6,5 @@ python scraper/scrape.py
 
 current_datetime=$(date '+%Y-%m-%d')
 echo "Dataset update time: $current_datetime" >> /var/log/trendy.log
-kaggle datasets version --dir-mode zip -p data -m "Dataset updated on ${current_datetime}"
+kaggle datasets version  -p data -m "Dataset updated on ${current_datetime}"
 echo "Script finished at $(date)" >> /var/log/trendy.log
